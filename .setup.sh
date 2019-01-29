ef0ec7e9a48e830cfb620652746ef635423daf28
Adam Hepton <adam@hepton.org>
2019-01-29 09:06:03 +0000

    Amended setup, removed conditionals, more automated

--- .setup.sh
+++ .setup.sh
@@ -1,5 +1,5 @@
-#!/bin/zsh
-DOTFILESREPO='https://bitbucket.org/pr0x/dotfiles.git'
+#!/bin/bash
+DOTFILESREPO='git@bitbucket.org:pr0x/dotfiles.git'
 git clone --bare "$DOTFILESREPO" "$HOME/.cfg"
 function config {
   $(which git) --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
@@ -15,19 +15,10 @@ fi;
 config checkout
 config config status.showUntrackedFiles no
 
+source "$HOME/.brew/brew-helpers.sh"
+"$HOME/.brew/brew-install.sh"
+
+zsh
 chsh -s $(which zsh)
-
-source .brew/brew-helpers.sh
-echo -n "Would you like to install all items from .brew/brew-install.sh?: (y/N)"
-read -n 1 DOINSTALL
-if [ "$DOINSTALL" -eq "Y" ] || [ "$DOINSTALL" -eq "y" ] ; then
-  ${DOTBREW}/brew-install.sh
-fi
-
-echo -n "Would you like to build .brew/brew-install.sh based on your existing config?: (y/N)"
-read -n 1 DOBUILD
-if [ "$DOBUILD" -eq "Y" ] || [ "$DOBUILD" -eq "y" ] ; then
-  update-brew-script
-fi
 
 echo "Basic install complete."

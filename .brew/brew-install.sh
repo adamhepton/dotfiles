ff36db44ed2f35d2cca69332e383dc1ddf3a2bc4
Adam Hepton <adam@hepton.org>
2020-05-26 15:44:02 +0100

    Updated brew-install.sh automatically

--- .brew/brew-install.sh
+++ .brew/brew-install.sh
@@ -143,7 +143,6 @@ brew install zsh-completions
 brew install zsh-syntax-highlighting
 
 echo "Adding casks"
-brew cask install boom
 brew cask install discord
 brew cask install docker
 brew cask install font-firacode-nerd-font
